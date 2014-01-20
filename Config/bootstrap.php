953b8bbe21cbfe98b31b5fbfb859d837f6ff6d3b
Mike Tallroth <mike.tallroth@gmail.com>
2014-01-19 22:42:37 -0600

    Force Clinks helper into all controllers, not just Nodes.

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,21 +1,19 @@
 <?php
-/**
- * Routes
- *
- */
-//	Croogo::hookRoutes('Clinks');
 
+/**
+ * Link confirmation message.
+ * This is shown when following a link with this option selected.
+ * TODO install this as a setting at plugin activation time.
+ */
 Configure::write('Clinks.confirmMessage',
 	'By clicking OK, you are leaving the ' . Configure::read('Site.title') . ' site. Please confirm.');
 
 /**
  * Helper
  *
- * This plugin's Example helper will be loaded via NodesController.
+ * The Clinks helper will be loaded into all controllers.
  */
-	Croogo::hookHelper('Nodes', 'Clinks.Clinks');
-//	Croogo::hookHelper('Contacts', 'Clinks.Clinks');
-//	Croogo::hookHelper('ClinkAttachments', 'Ckeditor.Ckeditor');
+	Croogo::hookHelper('*', 'Clinks.Clinks');
 	
 /**
  * TODO maybe make this conditional on some setting/option?
@@ -34,18 +32,21 @@ Configure::write('Wysiwyg.articleBrowseUrl', array(
 
 /**
  * Hook in the Ckeditor helper when browsing attachments.
+ * Force the use of a custom ckeditor config file which
+ * hooks in the custom ckedior clink plugin.
+ * Uses the basePath setting because at time of bootstrapping
+ * the Router class doesn't know the base path yet, so
+ * at Plugin enable time (in ClinksActivation), we capture
+ * and store the basePath.
  */
+$action_config = array(
+	0 => array(
+		'customConfig' => Configure::read('Clink.basePath') . 'clinks/js/ckeditor/config.js',
+	),
+);
 Croogo::mergeConfig('Wysiwyg.actions', array(
 	'ClinkAttachments/admin_browse',
 	'ClinkNodes/admin_browse',
-	'Nodes/admin_add' => array(
-		0 => array(
-			'customConfig' => Configure::read('Clink.basePath') . 'clinks/js/ckeditor/config.js',
-		),
-	),
-	'Nodes/admin_edit' => array(
-		0 => array(
-			'customConfig' => Configure::read('Clink.basePath') . 'clinks/js/ckeditor/config.js',
-		),
-	),
+	'Nodes/admin_add' => $action_config,
+	'Nodes/admin_edit' => $action_config,
 ));
